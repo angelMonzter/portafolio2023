37b833a4eeb0f61b7c00e0ec53e8513cb45fb437
Angel Efren Velazquez <angelmonztr2000@hotmail.com>
2022-11-26 12:44:53 -0600

    Maqueta completa

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation();
+window.onscroll = function() {
+  let y = window.scrollY;
+  const nav = document.querySelector('nav');
+  if (y > 450) {
+  	nav.style.position = 'fixed';
+  	nav.style.top = '5%';
+  }else{
+  	nav.style.position = 'absolute';
+  	nav.style.top = '55%';
+  }
+};
