57c220e687896baffbb0f61ed97aca41ca671bb6
Angel Efren Velazquez <angelmonztr2000@hotmail.com>
2022-12-03 11:36:48 -0600

    Portafolio responsive

--- js/app.js
+++ js/app.js
@@ -9,3 +9,34 @@ window.onscroll = function() {
   	nav.style.top = '55%';
   }
 };
+
+const mediaQueryLaptop = window.matchMedia('(max-width: 1500px)');
+const mediaQueryMovil = window.matchMedia('(max-width: 900px)');
+
+if (mediaQueryLaptop.matches) {
+  window.onscroll = function() {
+    let y = window.scrollY;
+    const nav = document.querySelector('nav');
+    if (y > 220) {
+      nav.style.position = 'fixed';
+      nav.style.top = '0%';
+    }else{
+      nav.style.position = 'absolute';
+      nav.style.top = '39%';
+    }
+  };
+}
+
+if (mediaQueryMovil.matches) {
+  window.onscroll = function() {
+    let y = window.scrollY;
+    const nav = document.querySelector('nav');
+    if (y > 220) {
+      nav.style.position = 'fixed';
+      nav.style.top = '0%';
+    }else{
+      nav.style.position = 'absolute';
+      nav.style.top = '23%';
+    }
+  };
+}
